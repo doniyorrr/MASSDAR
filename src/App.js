1e0426053a5f095f16fcb78aa9189805463c9dc0
Doniyor <elmurodovdoniyor589@gmail.com>
2022-04-06 14:54:45 +0500

    first step

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App(){
     <div>
       <button className="rounded-full bg-sky-500/50 py-2 px-4 m-5" onClick={notify}>Notify!</button>
       <ToastContainer />
+      <h1>MASSDAR</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab voluptates iure ea ipsum veritatis sint animi dolor soluta. Magnam tempore quibusdam debitis accusamus tenetur impedit a cumque itaque possimus in!</p>
     </div>
   );
 }
